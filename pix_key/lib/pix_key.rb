f58726daa28f47019ac9f29d6fa60c45849b2816
Priscilla Reges <ninja.pri@gmail.com>
2022-07-19 20:11:46 -0300

    Serradura

--- pix_key/lib/pix_key.rb
+++ pix_key/lib/pix_key.rb
@@ -1,143 +1,46 @@
 # frozen_string_literal: true
 
 class PixKey
-    attr_accessor :pix_key
+    module Type
+        EVP = 'evp'
+        CPF = 'cpf'
+        CNPJ = 'cnpj'
+        PHONE = 'phone'
+        EMAIL = 'email'
 
-    CPF = /^[0-9]{11}$/
-    CNPJ = /^[0-9]{14}$/
-    PHONE = /^\+[1-9][0-9]\d{1,14}$/
-    EMAIL = /^[a-z0-9.!#$&'*+\/=?^_`{|}~-]+@a-z0-9?(?:\.a-z0-9?)*$/
-    EVP = /[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}/
+        Detect = ->(str) do
+            return CPF if key.match?(/^[0-9]{11}$/)
+            return CNPJ if key.match?(/^[0-9]{14}$/)
+            return PHONE if key.match?(/^\+[1-9][0-9]\d{1,14}$/)
+            return EMAIL if key.match?(/^[a-z0-9.!#$&'*+\/=?^_`{|}~-]+@a-z0-9?(?:\.a-z0-9?)*$/)
+            return EVP if key.match?(/[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}/)
 
-    def initialize(pix_key)
-        @pix_key = pix_key
-    end
-
-    def to_s
-        @key = @pix_key.to_s
-    end
-
-    def value
-        cpf_value = @key =~ PixKey::CPF
-        cnpj_value = @key =~ PixKey::CNPJ
-        phone_value = @key =~ PixKey::PHONE
-        email_value = @key =~ PixKey::EMAIL
-        evp_value = @key =~ PixKey::EVP
-
-        #@pix_key = PixKey.new(key).to_s
-        @cpf = PixKey.new(cpf_value).to_s
-        @cnpj = PixKey.new(cnpj_value).to_s
-        @phone = PixKey.new(phone_value).to_s
-        @email = PixKey.new(email_value).to_s
-        @evp = PixKey.new(evp_value).to_s
-
-    end
-
-    def valid?
-        valid = [{key: '12312312334'}, 
-            {key: '12345678901234'}, 
-            {key: '+5510998765432'}, 
-            {key: 'email@husky.io'}, 
-            {key: '123e4567-e89b-12d3-a456-426655440000'}]
-
-        invalid = [{key: '1231231123122334'}, 
-            {key: 'invalid'}, 
-            {key: '551099876543299'}, 
-            {key: 'emailhusky.io'}, 
-            {key: '11123e45670sdsd00a-e21289b-12d3-a456-426655440000'}]
-
-        valid.each do |key|
-            return true if valid
+            ''
         end
 
-    end
-    def key
-        @pix_key.eql?(@key)
-
-    end
-
-    def type
-        type = {
-            'cpf' => '86110735094',
-            'cnpj' => '75928551000119',
-            'phone' => '+55998822334',
-            'email' => 'email@husky.io',
-            'evp' => '123e4567-e89b-12d3-a456-426655440000'
-        }
-        if type['cpf'] == '86110735094' 
-            'cpf'
-        end
-        if type['75928551000119'] == '75928551000119' 
-            'cnpj'
-        end
-        if type['+55998822334'] == '+55998822334' 
-            'phone'
-        end
-        if type['email@husky.io'] == 'email@husky.io' 
-            'email'
-        end
-        if type['123e4567-e89b-12d3-a456-426655440000'] == '123e4567-e89b-12d3-a456-426655440000' 
-            'evp'
-        end
-    end
-
-    def cpf?
-        key = '86110735094'
-        invalid_cpf = ['86110735099',
-            '861.107.350-94',
-            '+5582998899889',
-            'fernando@husky',
-            '123e4567-e89b-12d3-a456-426655440000']
-        invalid_cpf.include? key
-
-    end
-
-    def cnpj?
-        @pix_key = '75928551000119'
-        if @pix_key = '75928551000119'
-            true
-        end
-        invalid_cnpj = ['75.928.551/0001-19',
-            '75928551000118',
-            '861.107.350-94',
-            '+5582998899889',
-            'fernando@husky',
-            '123e4567-e89b-12d3-a456-426655440000']
-
-        invalid_cnpj.include? @pix_key
+    attr_reader :key, :type, :valid
 
 
-    end
-    def phone?
-        key = '+55861107350'
+    def initialize(arg)
+        @key = arg.is_a?(String) ? arg.strip.freeze : ''
+        @type = Type::Detect[@key]
+        @valid = !@type.empty?
+    end 
 
-        invalid_phone = ['55861107350',
-            'fernando@husky',
-            '123e4567-e89b-12d3-a456-426655440000']
-        invalid_phone.include? key
+    alias to_s key
+    alias value key
 
+    alias valid? valid
+    def invalid? = !valid
 
+    def evp? = type == Type::EVP
+    def cpf? = type == Type::CPF
+    def cnpj? = type == Type::CNPJ
+    def phone? = type == Type::PHONE
+    def email? = type == Type::EMAIL
 
-    end
-    def email?
-        @key
-
-        invalid_email = ['emailhusky.io',
-            '75.928.551/0001-19',
-            '861.107.350-94',
-            '+5582998899889',
-            '123e4567-e89b-12d3-a456-426655440000']
-        invalid_email.include? @key
-    end
-    def evp?
-        key = '123e4567-e89b-12d3-a456-426655440000'
-
-        invalid_evp = ['75.928.551/0001-19',
-            '861.107.350-94',
-            '+5582998899889',
-            'fernando@husky']
-        invalid_evp.include? key
-
+    def == (other)
+        other.class == self.class && other.key == key
     end
 
 end
